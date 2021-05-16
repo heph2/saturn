215182e6d8e8fc40f39cbc43194f132e699acecc
Heph <srht@mrkeebs.eu>
2021-05-16 13:24:47 +0200

    Added wrap over io.Reader for the progress bar

--- download.go
+++ download.go
@@ -1,29 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
-	"time"
 )
 
-// Small random progress bar
-func downProgress(delay time.Duration) {
-	for {
-		for _, r := range `\|/` {
-			fmt.Printf("\r%c", r)
-			time.Sleep(delay)
-		}
-	}
+type ReadSpy struct {
+	r  io.Reader
+	ch chan int
 }
+
+func (r *ReadSpy) Read(p []byte) (n int, err error) {
+	n, err = r.r.Read(p)
+	r.ch <- n
+	return
+}
+
+// func (r *ReadSpy) Close() error {
+// 	close(r.ch)
+// 	return r.r.Close()
+// }
 
 func Download(in <-chan Anime) {
 	for ep := range in {
-		go downProgress(100 * time.Millisecond)
-		nameFile := strings.ReplaceAll(ep.Name, " ", "") + ".mp4"
+		nameFile := strings.TrimSpace(ep.Name) + ".mp4"
 		out, _ := os.Create(nameFile)
 		defer out.Close()
 
@@ -31,9 +35,24 @@ func Download(in <-chan Anime) {
 		if err != nil {
 			log.Println(err)
 		}
-		defer res.Body.Close()
+		//		defer res.Body.Close()
 
-		if _, err := io.Copy(out, res.Body); err != nil {
+		contentLenght := res.Header.Get("Content-Length")
+		size, _ := strconv.ParseInt(contentLenght, 10, 64)
+
+		src := &ReadSpy{r: res.Body, ch: make(chan int)}
+		//		defer src.Close()
+
+		go func() {
+			var bar Bar
+			bar.NewOption(0, int(size))
+			for p := range src.ch {
+				bar.Play(int(p))
+			}
+		}()
+
+		_, err = io.Copy(out, src)
+		if err != nil {
 			log.Println(err)
 		}
 	}
